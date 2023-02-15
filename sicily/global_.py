2fc832a2552a30dfebe6c28756864eecb3ca5231
Anna Nikolaides <avvanik@Annas-MacBook-Air.local>
2023-02-15 16:38:49 +0800

    init

---
+++ sicily/global_.py
@@ -0,0 +1,19 @@
+lon_min = 13.78696
+lon_max = 15.62158
+lat_min = 37.76809
+lat_max = 38.49400
+
+base_url = 'https://api.opentripmap.com/0.1/en/places/'
+api_key = '5ae2e3f221c38a28845f05b656934aef7d36d2c813d4ee1bc9d86b1d'
+
+page_length = 250
+
+dict_detailed = {'xid': [],
+                 'name': [],
+                 'url': [],
+                 'stars': [],
+                 'wikipedia': [],
+                 'image': [],
+                 'address': [],
+                 'point': [],
+                 'kinds': []}
---
+++ sicily/main.py
@@ -0,0 +1,9 @@
+from model import Model
+
+
+def main():
+    Model().create_csv()
+
+
+if __name__ == "__main__":
+    main()
