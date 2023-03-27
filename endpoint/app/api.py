6b1d773786f9be73e57a4c294b3991eec7745558
Anna Nikolaides <avvanik@Annas-MacBook-Air.local>
2023-03-27 17:01:29 +0700

    api delete endpoints

--- endpoint/app/api.py
+++ endpoint/app/api.py
@@ -26,41 +26,27 @@ class Neo4jDB:
         result = tx.run("MATCH (p:POI) return p")
         return result.values()
 
-    @staticmethod
-    def return_kind(tx, kind):
-        result = tx.run("MATCH (p:POI {kind: $kind}) RETURN p", kind=kind)
-        return result.values
 
     @staticmethod
-    def nearest_poi(tx, kind, lon, lat):
-        result = tx.run("MATCH (p:POI {kind: $kind, longitude: $lon, latitude: $lat}) "
-                        " SET location: point({latitude: toFloat(p.lat), longitude: toFloat(p.lon)}"
-                        "RETURN p", kind=kind, lon=lon, lat=lat)
-        return result
+    def itinerary_proposal(tx, lon, lat):
+        # get closest poi as start node
+        start_node = tx.run("MATCH (p:POI) WITH p, point.distance(point({latitude: $lat, longitude: $lon, "
+                            "crs: 'wgs-84}) as distance ORDER BY distance ASC LIMIT 1", lon=lon, lat=lat)
 
-    @staticmethod
-    def create_map(poi):
+        # set the closest poi as start node and return itinerary (end node 2be defined)
+        result = tx.run("MATCH($start), (e:End), p = shortestPath((s)-[*]-(e)) "
+                        "WHERE length(p) > 1 RETURN p ", start=start_node)
+
+        # create map
         itinerary_map = folium.Map(location=[38.05968, 13.26699], zoom_start=13)
-        lat = poi.get('lat')
-        lon = poi.get('lon')
+        lat = result.get('lat')
+        lon = result.get('lon')
 
         for x, y in zip(lat, lon):
             folium.Marker(([x, y])).add_to(itinerary_map)
 
+        print(result)
         return 'output.html'
-
-    @staticmethod
-    def itinerary_proposal(tx, lon, lat):
-
-        # get closest poi
-        nearest_poi = tx.run("MATCH (p:POI) WITH p, point.distance(point({latitude: $lat, longitude: $lon, "
-                             "crs: 'wgs-84}) as distance ORDER BY distance ASC LIMIT 1", lon=lon, lat=lat)
-
-        # set the closest poi as start node and return itinerary (end node 2be defined)
-        result = tx.run("MATCH($nearest_poi), (e:End), p = shortestPath((s)-[*]-(e)) "
-                        "WHERE length(p) > 1 RETURN p ", nearest_poi=nearest_poi)
-
-        return result
 
 
 api = FastAPI(
@@ -82,19 +68,9 @@ def read_item():
         return session.write_transaction(db.return_data)
 
 
-@api.get("/poi/{kind:str}/{lon:float}/{lat:float}")
-def return_nearest_poi(kind, lon, lat):
-    with db.driver.session() as session:
-        return session.write_transaction(db.nearest_poi, kind, lon, lat)
-
-
 @api.get("/poi/itinerary/{lon:float}/{lat:float}")
 def return_itinerary(lon, lat):
     with db.driver.session() as session:
         return session.write_transaction(db.itinerary_proposal, lon, lat)
 
 
-@api.get("/poi/itinerary/map/{poi:str}")
-def return_itinerary_as_map(poi):
-    with db.driver.session() as session:
-        return session.write_transaction(db.create_map, poi)
